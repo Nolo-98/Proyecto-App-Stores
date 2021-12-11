77f8f0e07961ce8ebf3c12c356ad1492f7f8e0f7
Nolo-98 <twisterfast@gmail.com>
2021-12-11 00:56:15 -0600

    Eliminar un registro

--- app/src/main/java/com/cursosant/android/stores/OnClickListener.kt
+++ app/src/main/java/com/cursosant/android/stores/OnClickListener.kt
@@ -13,4 +13,5 @@ package com.cursosant.android.stores
 interface OnClickListener {
     fun onClick(storeEntity: StoreEntity)
     fun onFavoriteStore(storeEntity: StoreEntity)
+    fun onDeleteStore(storeEntity: StoreEntity)
 }
