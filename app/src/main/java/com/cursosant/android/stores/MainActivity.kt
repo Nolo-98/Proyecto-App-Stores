0c63089ef53905e4ebb1dc4633fdae5061227298
Nolo-98 <twisterfast@gmail.com>
2021-12-11 20:16:07 -0600

    Intent: Sitio Web

--- app/src/main/java/com/cursosant/android/stores/MainActivity.kt
+++ app/src/main/java/com/cursosant/android/stores/MainActivity.kt
@@ -104,7 +104,7 @@ class MainActivity : AppCompatActivity(), OnClickListener, MainAux {
 
                     1 -> dial(storeEntity.phone)
 
-                    2 -> Toast.makeText(this, "Sitio Web", Toast.LENGTH_SHORT).show()
+                    2 -> goToWebsite(storeEntity.website)
                 }
             })
             .show()
@@ -132,6 +132,19 @@ class MainActivity : AppCompatActivity(), OnClickListener, MainAux {
         }
 
         startActivity(callIntent)
+    }
+
+    private fun goToWebsite(website: String){
+        if (website.isEmpty()){
+            Toast.makeText(this, R.string.main_error_no_website, Toast.LENGTH_LONG).show()
+        } else {
+            val websiteIntent = Intent().apply {
+                action = Intent.ACTION_VIEW
+                data = Uri.parse(website)
+            }
+
+            startActivity(websiteIntent)
+        }
     }
 
     /*
